087c029ed14fa36c5276a8c0535584e6957160fb
Veronika Romashkina <vrom911@gmail.com>
2019-03-26 10:05:34 +0800

    [#17] Create projects context (#19)
    Resolves #17

---
+++ src/TechBlog/Project.hs
@@ -0,0 +1,33 @@
+module TechBlog.Project
+       ( Project (..)
+       , mkProjectCtx
+       ) where
+
+import Hakyll (Compiler, Context, Item, field, itemBody, listField, makeItem)
+
+data Project = Project
+    { pName :: String
+    , pLink :: String
+    , pImg  :: String
+    , pDesc :: String
+    }
+
+projectCtx :: Context Project
+projectCtx =
+    field "pName" (pure . pName . itemBody)
+ <> field "pLink" (pure . pLink . itemBody)
+ <> field "pImg"  (pure . pImg  . itemBody)
+ <> field "pDesc" (pure . pDesc . itemBody)
+
+allProjects :: Compiler [Item Project]
+allProjects = traverse makeItem
+    [ Project
+        { pName = "Elm Street"
+        , pLink = "elm-street"
+        , pImg  = ""
+        , pDesc = "🌳 Crossing the road between Haskell and Elm "
+        }
+    ]
+
+mkProjectCtx :: Context a
+mkProjectCtx = listField "projects" projectCtx allProjects
